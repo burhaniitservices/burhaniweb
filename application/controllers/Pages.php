7df8b03ae5e784533b93c8b7eac24339ecdb0155
harshan12 <harshan.shantiinfotech@gmail.com>
2018-09-15 22:54:39 +0530

    burhani web first commit

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -14,4 +14,32 @@ class Pages extends CI_Controller {
     $this->load->view('pages/'.$page);   
     $this->load->view('pages/template/footer');
   }
+  public function view()
+  {
+    # code...
+  }
+  public function show($id = "")
+  {
+    # code...
+  }
+  public function create()
+  {
+    # code...
+  }
+  public function store()
+  {
+    # code...
+  }
+  public function edit($id)
+  {
+    # code...
+  }
+  public function update()
+  {
+    # code...
+  }
+  public function delete($id)
+  {
+    # code...
+  }
 }
