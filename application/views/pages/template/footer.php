3c783c72be6b70cad6d3bdd0101b5e580452f7fa
harshan12 <harshan.shantiinfotech@gmail.com>
2018-09-16 17:45:13 +0530

    master commit

--- application/views/pages/template/footer.php
+++ application/views/pages/template/footer.php
@@ -1,4 +1,8 @@
     
     </div>
+    <script src="<?php echo base_url('assets/js/jquery.js');?>"></script>
+    <script src="<?php echo base_url('assets/js/materialize.min.js');?>"></script>
+    <script src="<?php echo base_url('assets/js/main.js');?>"></script>
+    <script src="<?php echo base_url('assets/js/jssor.slider.js');?>"></script>
   </body>
 </html>
